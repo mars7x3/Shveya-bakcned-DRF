f3de49afa8e2cb0f69934b2dc4de1df823323073
mars7x3 <m.ysakov.jcc@gmail.com>
2024-11-21 17:53:55 +0600

    warehouse end

--- app/serializers/user_crud.py
+++ app/serializers/user_crud.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
-from my_db.models import MyUser, StaffProfile, Rank, ClientProfile
+from my_db.enums import StaffRole
+from my_db.models import MyUser, StaffProfile, Rank, ClientProfile, Warehouse
 from serializers.general import RankSerializer
 
 
@@ -25,13 +26,22 @@ class MyUserUpdateSerializer(serializers.ModelSerializer):
         fields = ['username', 'password']
 
 
-class StaffSerializer(serializers.ModelSerializer):
-    user = MyUserSerializer(read_only=True)
-    rank = RankSerializer(read_only=True)
+class StaffWarehouseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Warehouse
+        fields = ['id', 'title', 'address']
 
+
+class StaffSerializer(serializers.ModelSerializer):
     class Meta:
         model = StaffProfile
         fields = '__all__'
+
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        if instance.role == StaffRole.WAREHOUSE:
+            rep['warehouse'] = StaffWarehouseSerializer(instance.warehouses.first(), context=self.context).data
+        return rep
 
 
 class StaffCreateUpdateSerializer(StaffSerializer):
