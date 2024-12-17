87780c533f2c19c24cf61f9af66953f5ecc65914
mars7x3 <m.ysakov.jcc@gmail.com>
2024-12-17 23:54:09 +0600

    fix

--- app/main_conf/celery.py
+++ app/main_conf/celery.py
@@ -1,22 +1,22 @@
-import os
-from django.conf import settings
-from celery import Celery
-from celery.schedules import crontab
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main_conf.settings')
-
-app = Celery('main_conf')
-app.config_from_object('django.conf:settings')
-
-app.autodiscover_tasks()
-
-app.conf.beat_schedule = {
-    'task_change_product_cost_price': {
-        'task': 'tasks.product.change_product_cost_price',
-        'schedule': crontab(hour=6, minute=0),
-    },
-
-}
-
-app.conf.timezone = settings.TIME_ZONE
-app.conf.update(result_extended=True)
+# import os
+# from django.conf import settings
+# from celery import Celery
+# from celery.schedules import crontab
+#
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main_conf.settings')
+#
+# app = Celery('main_conf')
+# app.config_from_object('django.conf:settings')
+#
+# app.autodiscover_tasks()
+#
+# app.conf.beat_schedule = {
+#     'task_change_product_cost_price': {
+#         'task': 'tasks.product.change_product_cost_price',
+#         'schedule': crontab(hour=6, minute=0),
+#     },
+#
+# }
+#
+# app.conf.timezone = settings.TIME_ZONE
+# app.conf.update(result_extended=True)
