7f49100c7a71d790e2e3340a3f1492a482eb92c8
mars7x3 <m.ysakov.jcc@gmail.com>
2025-03-20 16:42:41 +0600

    fix

--- app/main_conf/settings.py
+++ app/main_conf/settings.py
@@ -160,8 +160,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=30),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=90),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': False,
     'UPDATE_LAST_LOGIN': False,
